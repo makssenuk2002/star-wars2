725e3f859e3fac1972458bcb59c430a556406807
Maks <makssenuk2002@gmail.com>
2022-04-13 18:50:52 +0300

    added routes

---
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+
+
+import styles from './PeoplePage.module.css';
+import {getApiResource} from "../../utils/network";
+import {getPeopleId, getPeopleImage} from "../../services/getPeopleData";
+import {API_PEOPLE} from "../../constants/api";
+import PeopleList from "../../components/PeoplePage/PeopleList";
+import PropTypes from "prop-types";
+import {withErrorApi} from "../../hoc-helpers/withErrorApi";
+
+const PeoplePage = ({ setErrorApi }) => {
+    const [people, setPeople] = useState(null);
+
+    const getResource = async (url) => {
+        const res = await getApiResource(url);
+
+        if (res) {
+            const peopleList = res.results.map(({ name, url }) => {
+                const id = getPeopleId(url);
+                const img = getPeopleImage(id);
+    
+                return {       
+                    id,
+                    name,
+                    img
+                }
+            })
+            
+            setPeople(peopleList);
+            setErrorApi(false);
+        } else {
+            setErrorApi(true);
+        }
+    }
+
+    useEffect(() => {
+        getResource(API_PEOPLE);
+    }, []);
+
+    return (
+        <>
+            <h1 className="header__text">Navigation</h1>
+            {people && <PeopleList people={people} />}
+        </>
+    )
+}
+
+PeoplePage.propTypes = {
+    setErrorApi: PropTypes.func
+}
+
+export default withErrorApi(PeoplePage);
